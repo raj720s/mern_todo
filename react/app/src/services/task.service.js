4921eef0466e83bede6b36227112e392150e7ddb
raj720s <raj720s@raj.com>
2023-09-03 03:35:01 +0530

    :3rd

--- react/app/src/services/task.service.js
+++ react/app/src/services/task.service.js
@@ -1,4 +1,4 @@
-const api = 'https://node-for-mern.onrender.com:3500/api'
+const api = 'https://node-for-mern.onrender.com/api'
 // const api = 'http://localhost:3500/api'
 export const getAllTasks = () => {
   return new Promise((res, rej) => {
